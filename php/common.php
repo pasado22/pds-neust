7699fff7ab67ceb1bd390aaba89850089ed11a7d
Alger <aljer2242@gmail.com>
2022-03-23 09:43:36 +0800

    modfied php/common.php & php/conn.php converted from mysqli to pdo registration

--- php/common.php
+++ php/common.php
@@ -5,57 +5,63 @@ session_start();
 
 // user registration
 if (isset($_POST['register_btn'])) {
-    $user_id = bin2hex(random_bytes(11));
-    $user_fname = mysqli_escape_string($conn, $_POST['user_fname']);
-    $user_mname = mysqli_escape_string($conn, $_POST['user_mname']);
-    $user_sname = mysqli_escape_string($conn, $_POST['user_sname']);
-    $user_email = mysqli_escape_string($conn, $_POST['user_email']);
-    $user_pass = mysqli_escape_string($conn, $_POST['user_pass']);
-    $user_phone = mysqli_escape_string($conn, $_POST['user_phone']);
+    $u_id = bin2hex(random_bytes(11));
+    $user_fname = $_POST['user_fname'];
+    $user_mname = $_POST['user_mname'];
+    $user_sname = $_POST['user_sname'];
+    $user_email = $_POST['user_email'];
+    $user_pass = $_POST['user_pass'];
+    $user_phone = $_POST['user_phone'];
+
     clean_data($user_fname, $user_mname, $user_sname, $user_email, $user_pass, $user_phone);
 
-    $stmt = "INSERT INTO `user_main_tbl` (`user_id`, `user_fname`, `user_mname`, `user_sname`, `user_email`, `user_pass`, `user_phone`, `created`, `modfied`) 
-            VALUES ('$user_id', '$user_fname', '$user_mname', '$user_sname', '$user_email', '$user_pass', '$user_phone', NOW(), NULL)";
-    $qry = mysqli_query($conn, $stmt);
-
-    if ($qry != false) {
-        $_SESSION['msg'] = 2;
-        header("Location: ../register.php");
-    } else if ($qry == false) {
-        die("Error: " . mysqli_error($conn) . " $stmt");
-    } else {
-        $_SESSION['msg'] = 1;
-        header("Location: ../register.php");
-    };
+    try {
+        $insert = $conn->prepare("INSERT INTO `user_main_tbl` (`user_id`, `user_fname`, `user_mname`, `user_sname`, `user_email`, `user_pass`, `user_phone`, `created`, `modified`) 
+        VALUES (:u_id, :user_fname, :user_mname, :user_sname, :user_email, :user_pass, :user_phone, NOW(), NULL)");
+        $insert->execute([
+            'u_id' => $u_id,
+            ':user_fname' => $user_fname,
+            ':user_mname' => $user_mname,
+            ':user_sname' => $user_sname,
+            ':user_email' => $user_email,
+            ':user_pass' => $user_pass,
+            ':user_phone' => $user_phone
+        ]);
+    } catch (PDOException $e) {
+        echo $insert . "<br>" . $e->getMessage();
+    } finally {
+        $insert = NULL; $conn = NULL;
+    }
+    $_SESSION['msg'] = 2;
+    header("Location: ../index.php");
 }
 
 // login
-if (isset($_POST['login_btn'])) {
-    $user_email = mysqli_escape_string($conn, $_POST['user_email']);
-    $user_pass = mysqli_escape_string($conn, $_POST['user_pass']);
-    clean_data($user_email, $user_pass);
+// if (isset($_POST['login_btn'])) {
+//     $user_email = $_POST['user_email'];
+//     $user_pass = $_POST['user_pass'];
+//     clean_data($user_email, $user_pass);
 
-    $stmt = "SELECT * FROM `admin_tbl` WHERE `admin_name`='$user_email' AND `admin_pass`='$user_pass'";
-    $qry = mysqli_query($conn, $stmt);
-    $row = mysqli_fetch_array($qry);
+//     $stmt = "SELECT * FROM `admin_tbl` WHERE `admin_name`='$user_email' AND `admin_pass`='$user_pass'";
+//     $qry = mysqli_query($conn, $stmt);
+//     $row = mysqli_fetch_array($qry);
 
-    if ("admin" == $user_email && "admin" == $user_pass) {
-        $_SESSION['admin_id'] = $row['admin_id'];
-        $_SESSION['msg'] = 4;
-        header("Location: ../page_admin/index.php");
-    }
+//     if ("admin" == $user_email && "admin" == $user_pass) {
+//         $_SESSION['admin_id'] = $row['admin_id'];
+//         $_SESSION['msg'] = 4;
+//         header("Location: ../page_admin/index.php");
+//     }
 
-    $stmt = "SELECT * FROM `user_main_tbl` WHERE `user_email`='$user_email' AND `user_pass`='$user_pass'";
-    $qry = mysqli_query($conn, $stmt);
-    $row = mysqli_fetch_array($qry);
+//     $stmt = "SELECT * FROM `user_main_tbl` WHERE `user_email`='$user_email' AND `user_pass`='$user_pass'";
+//     $qry = mysqli_query($conn, $stmt);
+//     $row = mysqli_fetch_array($qry);
 
-    if (mysqli_num_rows($qry) == 1) {
-        $_SESSION['uid'] = $row['user_id'];
-        header("Location: ../page_user/index.php");
-    }
-}
+//     if (mysqli_num_rows($qry) == 1) {
+//         $_SESSION['uid'] = $row['user_id'];
+//         header("Location: ../page_user/index.php");
+//     }
+// }
 
 
 // redirect unauthorized access
-$_SESSION['msg'] = 1;
-header("Location: ../index.php");
+
--- php/conn.php
+++ php/conn.php
@@ -4,12 +4,13 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "pds_db";
 
-// Create connection
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+try {
+  $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
 ?>
