931f292eda7664558ff5506fe232f04e2da45610
Alger <aljer2242@gmail.com>
2022-03-25 18:23:11 +0800

    modify: index&pds added pds2&udry

--- page_user/index.php
+++ page_user/index.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+if ($row['verify'] != true) {
+    header("location: ");
+} else if ($uid == false) {
+    $_SESSION['msg'] = 6;
+    header("location: ../index.php");
+}
+
 echo $uid = $_SESSION['uid'];
 echo "UWU <a href='../index.php'>Go back</a>";
 ?>
