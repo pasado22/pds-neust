ff7b0a41dc5060d44deb4bc5eacd19be9fc80d67
Alger Bedaña <69410170+alger24@users.noreply.github.com>
2022-04-30 15:13:21 +0800

    added ignore file

--- test.php
+++ test.php
@@ -1,8 +1,30 @@
 <?php
 require 'php/conn.php';
 require 'php/func.php';
+session_start();
 
+$uid = bin2hex(random_bytes(11));
 
-multi_insert('107dc8e30ed');
+try {
+    $conn->beginTransaction();
 
-?>
+    
+
+    $sql = "
+        INSERT INTO `user_lnd_tbl` (`user_id`) VALUES (:userid);
+        
+        ";
+
+    $insert = $conn->prepare($sql);
+    $insert->execute([
+        ':userid' => $uid,
+    ]);
+
+    $conn->commit();
+} catch (PDOException $e) {
+    echo "<br>" . $e->getMessage();
+} finally {
+    $conn = NULL;
+}
+
+    // header("Location: ../index.php?success=You're registered! YAY!");
